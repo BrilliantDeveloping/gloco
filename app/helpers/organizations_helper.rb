fbdffec89250b1aa55aba06f84b3add88fe2d203
Ricky <mail@brilliantdeveloping.com>
2015-01-26 14:06:36 -0800

    nav & org helper

--- app/helpers/organizations_helper.rb
+++ app/helpers/organizations_helper.rb
@@ -3,8 +3,9 @@ module OrganizationsHelper
     if action_name == 'new'
       image_tag("org_nologo.png", class: css)
     else
-      org.image.blank? ? image_tag("#{org.category.downcase}_nologo.png", 
-      class: css) : image_tag(org.image_url.to_s, class: css)
+      org.is_a?(String) ? image_tag("#{org.downcase}_nologo.png", class: css) : 
+      (org.image.blank? ? image_tag("#{org.category.downcase}_nologo.png", 
+      class: css) : image_tag(org.image_url.to_s, class: css))
     end
   end
 end
